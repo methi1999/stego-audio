53c7f4d82c98c39550cb71201ae2de246bceaed9
Mithilesh Vaidya <vaidya.mithilesh@gmail.com>
2021-05-10 12:17:17 +0530

    Added timit test, walter white results and file for plotting

--- speech/utils/plot.py
+++ speech/utils/plot.py
@@ -3,17 +3,46 @@ from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
 import pickle
 
-_, orig = read('rec_3.wav')
-_, new = read('rec_3_asr.wav')
-print(new.max())
-new = new*(2**15)
-delta = new - orig
-print(orig, new)
-plt.plot(orig, alpha=0.5, label='Original')
-plt.plot(delta, label='Perturbation')
-plt.xlabel("Sample")
-plt.ylabel("Amplitude")
-plt.legend()
-plt.title("Original and Perturbation\nRecording 3")
 
-plt.show()
+def pesq_loss_edit_iter(pickle_path, title):
+    loss, pesq, edit = pickle.load(open(pickle_path, 'rb'))
+    # pesq
+    iters = [x[0] for x in pesq]
+    wb, nb = [x[1] for x in pesq], [x[2] for x in pesq]
+    plt.plot(iters, wb, color='b', label='wide-band', marker='x')
+    plt.plot(iters, nb, color='r', label='narrow-band', marker='x')
+    plt.title(title)
+    plt.xlabel("Iterations")
+    plt.ylabel("PESQ score")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    # edit
+    # plt.plot([x[0] for x in edit], [x[1] for x in edit], color='g')
+    # plt.title(title)
+    # plt.xlabel("Iterations")
+    # plt.ylabel("Edit distance")
+    # plt.grid(True)
+    # plt.legend()
+    # plt.show()
+
+
+def plot_noise():
+    _, orig = read('rec_3.wav')
+    _, new = read('rec_3_asr.wav')
+    print(new.max())
+    new = new*(2**15)
+    delta = new - orig
+    print(orig, new)
+    plt.plot(orig, alpha=0.5, label='Original')
+    plt.plot(delta, label='Perturbation')
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.title("Original and Perturbation\nRecording 3")
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    pesq_loss_edit_iter('../../recordings/destroyer_rand.pkl', '')
--- stego.py
+++ stego.py
@@ -4,14 +4,14 @@ import pickle
 import torch
 import torch.nn as nn
 import torch.optim
-
+import numpy as np
 import torchaudio
 import speech
 from speech.utils.io import wave_from_array
 from speech.utils.io import array_from_wave
 import speech.models as models
 from speech.utils.score import pesq_score, compute_cer
-
+import argparse
 import tensorboard_logger as tb
 
 
@@ -67,11 +67,13 @@ def stego_spectro(config, audio_pth='tests/test1.wav', target_text=('aa', 'jh'))
                 inverse_delta(config, to_feed, preproc, name='spectro_hypo')
 
 
-def stego_audio(config_full, add_noise, audio_pth='tests/timit.wav',
-                target_text=('hh', 'ah', 'l', 'ow', 'sil', 'w', 'er', 'l', 'd')):
+def stego_audio(config_full, audio_pth, add_noise, inp_target, is_text, dump_suffix=''):
     # config['audio'] contains hop, window and fs params
+    if add_noise:
+        dump_suffix += '_noise'
     config = config_full['audio']
     hypo_path = audio_pth[:-4] + '_hypo.wav'
+    pkl_path = audio_pth[:-4] + '_{}.pkl'.format(dump_suffix)
     # load model
     model, _, preproc = speech.load("ctc_best", tag="best")
     # Freeze model params. Even if we don't, it doesnt matter since optimiser has only been passed delta as param
@@ -88,15 +90,20 @@ def stego_audio(config_full, add_noise, audio_pth='tests/timit.wav',
     print("Decoded text in audio: {}\nDecoded Sequence: {}".format(preproc.decode(out), out))
     # define delta
     delta = nn.Parameter(torch.zeros_like(orig), requires_grad=True).float()
-    target = torch.tensor(preproc.encode(target_text)).unsqueeze(0)
-    target_list = target[0].tolist()
+    if is_text:
+        target = torch.tensor(preproc.encode(inp_target)).unsqueeze(0)
+        target_list = target[0].tolist()
+    else:
+        target = torch.tensor(inp_target).unsqueeze(0)
+        target_list = list(inp_target)
     print("Target to encode:", target[0])
-    edit_dists = [compute_cer([(target_list, out)])]
+
+    edit_dists = [(0, compute_cer([(target_list, out)]))]
     # parameters
     thresh = orig.max()/3
     thresh_decay = 0.75
     check_every = 50
-    num_iter = 30000
+    num_iter = 10000
     # RMS/3 ToDo: convert to SNR based calculation
     if add_noise:
         noise_sigma = torch.sqrt(torch.mean(orig**2)).item()/3
@@ -127,10 +134,10 @@ def stego_audio(config_full, add_noise, audio_pth='tests/timit.wav',
             wave_from_array(to_write, fs, hypo_path)
             # store edit distance
             edit_dists.append((e, compute_cer([(target_list, cur_out)])))
-            print(edit_dists)
+
             if cur_out == target_list:
                 print("Got target output")
-                best_path = audio_pth[:-4] + '_enc_' + str(e) + '.wav'
+                best_path = audio_pth[:-4] + '_' + '_'.join([str(e), dump_suffix]) + '.wav'
                 wave_from_array(to_write, fs, best_path)
                 thresh = thresh_decay*min(thresh, d_max)
                 pesq_nb = pesq_score(orig.numpy(), to_write.numpy(), fs, 'nb')
@@ -139,7 +146,6 @@ def stego_audio(config_full, add_noise, audio_pth='tests/timit.wav',
                 logs.append((e, pesq_nb, pesq_wb))
 
                 # dump data
-                pkl_path = audio_pth[:-4] + '_data.pkl'
                 with open(pkl_path, 'wb') as f:
                     pickle.dump((losses, logs, edit_dists), f)
 
@@ -153,11 +159,20 @@ def stego_audio(config_full, add_noise, audio_pth='tests/timit.wav',
         if e % lr_step == 0:
             lr_scheduler.step()
 
+        if e % 200 == 0:
+            with open(pkl_path, 'wb') as f:
+                pickle.dump((losses, logs, edit_dists), f)
+
+
+    # dump data
+    with open(pkl_path, 'wb') as f:
+        pickle.dump((losses, logs, edit_dists), f)
+
     return losses, logs, edit_dists
 
 
-# def pesq_vs_iter():
-
+def get_random(length):
+    return np.random.randint(low=0, high=48, size=length).tolist()
 
 
 if __name__ == "__main__":
@@ -166,17 +181,44 @@ if __name__ == "__main__":
     with open(cfg_path, 'r') as fid:
         config = json.load(fid)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a")
+    args = parser.parse_args()
+
     random.seed(config["seed"])
     torch.manual_seed(config["seed"])
     tb.configure(config["save_path"])
     # use_cuda = torch.cuda.is_available()
     use_cuda = False
 
-    target = ('sil', 'ao', 't', 'ah', 'm', 'ae', 't', 'ih', 'k', 'sil', 's', 'p', 'iy', 'ch', 'sil', 'r', 'eh', 'k', 'ah',
-              'g', 'n', 'uh', 'sh', 'ah', 'n', 'sil')
-    # stego_spectro(config)
-    stego_audio(config, add_noise=True, audio_pth='recordings/destroyer.wav', target_text=target)
+    # nuclear strike authorised
+    s1 = "n uw k l iy er sil s t r ay k sil ao th er ay z d sil".split(' ')
+    # shyam, how is your semester exchange
+    s2 = 'sh y aa m sil hh aw ih z sil y ao r sil s ah m eh s t er sil ih k s ch ey n jh sil'.split(' ')
+    # Hey Alexa, add a TV to my shopping list
+    s3 = 'hh ey ah l eh k s ah sil ae d ah t iy v iy t uw sil m ay sh aa p ih ng l ih s t'.split(' ')
+    # code red
+    s4 = 'k ow d sil r eh d'.split(' ')
 
+    # make sets of examples
+    egs = [('recordings/test.wav', s1, 'nuclear', True), ('recordings/test.wav', get_random(15), 'rand', False),
+           ('recordings/walter.wav', s2, 'shyam', True), ('recordings/walter.wav', get_random(15), 'rand', False),
+           ('recordings/destroyer.wav', s4, 'code_red', True), ('recordings/destroyer.wav', get_random(40), 'rand', False)]
+
+    if args.a == 'all':
+        for rec_pth, to_enc, suffix, is_text in egs:
+            # stego_spectro(config)
+            stego_audio(config, audio_pth=rec_pth, add_noise=False, inp_target=to_enc, is_text=is_text, dump_suffix=suffix)
+    else:
+        idx = int(args.a)
+        stego_audio(config, audio_pth=egs[idx][0], add_noise=False, inp_target=egs[idx][1], is_text=egs[idx][3], dump_suffix=egs[idx][2])
+
+
+    # automatic speech recognition
+    # target = (
+    #     'sil', 'ao', 't', 'ah', 'm', 'ae', 't', 'ih', 'k', 'sil', 's', 'p', 'iy', 'ch', 'sil', 'r', 'eh', 'k', 'ah',
+    #     'g', 'n', 'uh', 'sh', 'ah', 'n', 'sil'
+    # )
     # can get phase from audio by setting power=None in spectrogram but GriffinLim does not accept it as input
     # so no point, otherwise we find other functions/libraries which can invert spectrogram
 
